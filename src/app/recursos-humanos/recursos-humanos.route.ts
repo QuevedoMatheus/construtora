6c2b4dc69a96e5bba7cacf6132dcf3c97401ae21
Eduardo Vasconcelos <107057997+EduardoVasconceloss@users.noreply.github.com>
2024-07-03 03:53:37 -0300

    Delete src directory

--- src/app/recursos-humanos/recursos-humanos.route.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from "@angular/router";
-import { FormBasicoComponent } from "./form-basico/form-basico.component";
-
-export const RH_ROUTE: Route[] = [
-  {path: 'recursos-humanos/cadastrar', component: FormBasicoComponent}
-
-];
-
-//teste
